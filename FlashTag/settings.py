f7e8d0967b096128c908716fdd46e5b5fe264013
M V V S Manoj Kumar <mvvsmanojkumar@gmail.com>
2023-11-28 09:51:06 +0530

    changed db

--- FlashTag/settings.py
+++ FlashTag/settings.py
@@ -85,20 +85,22 @@ WSGI_APPLICATION = 'FlashTag.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if not DEBUG:
-    DATABASES = {
-	"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
-
-
-else:
-
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+DATABASES = {
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
+
+# if not DEBUG:
+#     DATABASES = {
+#     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#     }
+# else:
+
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
 
 # DATABASES = {
