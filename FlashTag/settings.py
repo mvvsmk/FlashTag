692a69c7d9b989ec956c236c7be6808293d57c1b
M V V S Manoj Kumar <mvvsmanojkumar@gmail.com>
2023-12-10 13:42:15 +0530

    DEBUG OFF

--- FlashTag/settings.py
+++ FlashTag/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get("DEBUG", default=True) == True
+DEBUG = os.environ.get("DEBUG", default=True) == True
 
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 # ALLOWED_HOSTS = ["*"]
